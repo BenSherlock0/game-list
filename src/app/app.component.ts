d5b6a6d7ea7bd6cc58adc150753d6364837de196
Paul Sherlock <bensherlock0@gmail.com>
2020-05-03 15:59:27 +0100

    Bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { RawgApiService } from './services/rawg-api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [RawgApiService]
 })
+
 export class AppComponent {
-  title = 'game-list';
+  
+
+  gameData:RAWGResponse;
+  errorMessage:any;
+
+  constructor(private _rawgService:RawgApiService){
+
+  }
+
+  getGameDetails(gameName:string) : boolean{
+    this._rawgService.getGameData(gameName).subscribe(
+      gameData => {
+          this.gameData=gameData;
+          console.log('Title:' + this.gameData.Name)
+      },
+      error => this.errorMessage = <any>error
+    );
+    return false;
+  }
 }
